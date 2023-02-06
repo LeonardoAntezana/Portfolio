bc027781711d5016e38b0be1bdcbc97ebff11562
¨Leonardo <leonardoantezana59@gmail.com>
2023-02-06 10:40:23 -0300

    cambiando de estilo segun la validacion de datos

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -32,7 +32,7 @@ const Form = () => {
   const validation = (e) => {
     e.preventDefault()
     if(mail.error === false && mail.content != '' && asunto.error === false && content.error === false){
-      messaje('Informacion correcta')
+      messaje('Mensaje enviado!')
       let datos = {name: mail.content, asunto: asunto.content, content:content.content}
       emailjs.send('service_7x7hh0n', 'template_4s76swr', datos)
       setMail({content: '', error: false})
@@ -48,15 +48,15 @@ const Form = () => {
     <form id="contacto" onSubmit={validation} className={styles.form}>
       <h2>Contacto</h2>
       <div className={styles.conteiner__input}>
-        <input onBlur={() => handleBlur(val, setMail, mail.content)} onChange={(event) => handleChange(event, setMail)} value={mail.content} style={{border: `${mail.error ? '1px solid red': '1px solid green'}`}} type="email" placeholder="Correo Electrónico"/>
+        <input className={mail.error ? styles.input__error : undefined} onBlur={() => handleBlur(val, setMail, mail.content)} onChange={(event) => handleChange(event, setMail)} value={mail.content} type="email" placeholder="Correo Electrónico"/>
         {mail.error && <p>Campo erróneo</p>}
       </div>
       <div className={styles.conteiner__input}>
-        <input onBlur={() => handleBlur(whiteS, setAsunto, asunto.content)} onChange={(event) => handleChange(event, setAsunto)} value={asunto.content} style={{border: `${asunto.error ? '1px solid red': '1px solid green'}`}} type="text" placeholder="Asunto" maxLength={15}/>
+        <input className={asunto.error ? styles.input__error : undefined} onBlur={() => handleBlur(whiteS, setAsunto, asunto.content)} onChange={(event) => handleChange(event, setAsunto)} value={asunto.content} type="text" placeholder="Asunto" maxLength={15}/>
         {asunto.error && <p>Campo erróneo</p>}
       </div>
       <div className={styles.conteiner__input}>
-        <textarea onBlur={() => handleBlur(whiteS, setContent, content.content)} onChange={(event) => handleChange(event, setContent)} value={content.content} style={{border: `${content.error ? '1px solid red': '1px solid green'}`}} placeholder="Mensaje" rows={8}></textarea>
+        <textarea className={content.error ? styles.input__error : undefined} onBlur={() => handleBlur(whiteS, setContent, content.content)} onChange={(event) => handleChange(event, setContent)} value={content.content} placeholder="Mensaje" rows={8}></textarea>
         {content.error && <p>Campo erróneo</p>}
       </div>
       <span>
